89123e6313acd31abf15af0bfd3e6c364f08b180
Tom-Lee-CN <tomweichat@qq.com>
2025-06-16 13:18:24 +0800

    first commit

---
+++ docs/.vuepress/.temp/git/config.js
@@ -0,0 +1,9 @@
+import { GitContributors } from "D:/工作空间/1.项目/my-blog/vuepress-starter/node_modules/@vuepress/plugin-git/lib/client/components/GitContributors.js";
+import { GitChangelog } from "D:/工作空间/1.项目/my-blog/vuepress-starter/node_modules/@vuepress/plugin-git/lib/client/components/GitChangelog.js";
+
+export default {
+  enhance: ({ app }) => {
+    app.component("GitContributors", GitContributors);
+    app.component("GitChangelog", GitChangelog);
+  },
+};
---
+++ docs/.vuepress/.temp/pages/category/history/index.html.js
@@ -0,0 +1,3 @@
+import comp from "D:/工作空间/1.项目/my-blog/vuepress-starter/docs/.vuepress/.temp/pages/category/history/index.html.vue"
+const data = JSON.parse("{\"path\":\"/category/history/\",\"title\":\"Category History\",\"lang\":\"en-US\",\"frontmatter\":{\"title\":\"Category History\",\"sidebar\":false,\"blog\":{\"type\":\"category\",\"name\":\"History\",\"key\":\"category\"},\"layout\":\"Category\"},\"headers\":[],\"git\":{},\"filePathRelative\":null,\"excerpt\":\"\"}")
+export { comp, data }
